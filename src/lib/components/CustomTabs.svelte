1013ff98363cebafcf0afaa422d0686b203d7e6d
Murilo Matsubara <murilo.matsubara@gmail.com>
2024-02-22 19:06:58 -0300

    feat: adiciona tab de resultados

---
+++ src/lib/components/CustomTabs.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" generics="T extends string">
+	export let tabs: readonly T[];
+	export let selected: T;
+</script>
+
+<div class="flex">
+	{#each tabs as tab (tab)}
+		<button
+			class="border-b border-b-transparent px-8 py-3 transition-all hover:border-b-gray-400 {tab ===
+			selected
+				? 'border-b !border-b-primary font-semibold'
+				: ''}"
+			on:click={() => {
+				selected = tab;
+			}}>{tab}</button
+		>
+	{/each}
+</div>
